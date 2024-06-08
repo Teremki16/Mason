f533edce6315edcbdcd89974f052a97be8ea2fcc
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2024-06-08 17:49:06 +0300

    кнопочкі прікольні

--- script.js
+++ script.js
@@ -1,45 +1,69 @@
 
-$(document).ready(function () {
-  let Notes = [];
-  $("#add").on("click", function () {
-    let title = $("#addNotatkaPole").val()
-    let Note = $("#pole").val()
-    let Data = new Date().getTime()
-    let note = {
-      title: title,
-      text: Note,
-      date: Data,
-      isCompleted: false
-    }
-    Notes.push(note);
-    console.log(Notes)
-    $("#addNotatkaPole").val("")
-    $("#pole").val("")
-    draw()
-  })
+let Notes = [];
 
-
-  function draw() {
-    Notes.forEach(element => {
-      let notatka = `
-    <div class="note">
-      <div class="zagal">${element.title}</div>
-      <div class="notatka">${element.text}</div>
-      <div class="date">${element.date}</div>
-      <button class="edit"><i class="fa-solid fa-pen-to-square"></i></button>
-      <button class="archiv"><i class="fa-solid fa-check"></i></button>
-      <button class="delete"><i class="fa-solid fa-trash"></i></button>
-    </div>
-  `
-      $(".notes").append(notatka)
-    });
+$("#add").on("click", function () {
+  let title = $("#addNotatkaPole").val()
+  let Note = $("#pole").val()
+  let Data = new Date().getTime()
+  let note = {
+    title: title,
+    text: Note,
+    date: Data,
+    isCompleted: false
   }
+  Notes.push(note);
+  console.log(Notes)
+  $("#addNotatkaPole").val("")
+  $("#pole").val("")
+  draw()
+})
 
 
+function draw() {
+  $(".notes").empty()
+  
+  Notes.forEach(element => {
+    let notatka = `
+    <div class="note ${element.isCompleted}">
+    <div class="zagal">${element.title}</div>
+    <div class="notatka">${element.text}</div>
+    <div class="date">${new Date(element.date).toLocaleString()}</div>
+    <button class="edit"><i class="fa-solid fa-pen-to-square"></i></button>
+    <button class="archiv" onclick="find(${element.date})"><i class="fa-solid fa-check"></i></button>
+    <button class="delete" onclick="del(${element.date})"><i class="fa-solid fa-trash"></i></button>
+    </div>
+    `
+    $(".notes").append(notatka)
+  });
+  localStorage.setItem("note", JSON.stringify(Notes))
+}
 
 
+function del(d) {
+  Notes = Notes.filter(obj => obj.date != d);
+  console.log(d)
+  draw();
+}
+
+function find(idToFind) {
+  let objectToUpdate = Notes.find(obj => obj.date === idToFind);
+  if (objectToUpdate) {
+    objectToUpdate.isCompleted = !objectToUpdate.isCompleted;
+  }
+  console.log(Notes)
+  draw();
+}
+
+$("#clear").on("click", function(){
+  Notes = [];
+  draw();
+})
 
 
+let load = JSON.parse(localStorage.getItem("note")) 
+if(load != null){
 
+ Notes = load
+ draw()
 
-});
+}
